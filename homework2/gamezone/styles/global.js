6ce70fa62a91cc9a1b8a4757513b19c5f3b6e4f9
jackwestel <orlynao19@gmail.com>
2020-10-16 16:25:24 -0400

    homework submission

--- homework2/gamezone/styles/global.js
+++ homework2/gamezone/styles/global.js
@@ -2,7 +2,6 @@ import { StyleSheet } from 'react-native';
 
 export const globalStyles = StyleSheet.create({
   titleText: {
-    /*fontFamily: 'nunito-bold',*/
     fontSize: 18,
     fontWeight: 'bold',
     color: '#333',
